312eb2a6ae698a9bc3711dc1e596848821dfe8ff
seoyoona <synsyn0311@gmail.com>
2023-10-15 23:12:36 +0900

    tried scroll js - failed

--- basic.js
+++ basic.js
@@ -73,5 +73,75 @@ window.addEventListener('scroll', function(e) {
       });
       ticking = true;
    }
+
+   
 });
 
+
+// const contents = $('.b_box');
+// let secArr = [];
+// contents.each((index) => {
+// 	secArr.push(index);
+// });
+// let w;
+// let h;
+// $(window).resize(function () {
+// 	w = $(window).width();
+// 	h = $(window).height();
+// 	contents.height(h);
+// 	contents.width(w);
+// 	$('.b_content').height(h);
+// });
+// $(window).trigger('resize');
+// // 초기 위치 설정
+// $('html,body')
+// 	.stop()
+// 	.animate({ scrollLeft: contents.eq(1).offset().left }, 1300);
+// contents.on('wheel', function (event) {
+// 	let currentIndex = $(this).index();
+
+// 	if (event.originalEvent.deltaY > 0) {
+// 		// 마우스 휠 아래로 스크롤
+// 		currentIndex++;
+// 		if (currentIndex >= secArr.length) {
+// 			// 마지막 요소를 넘어가면 처음 요소로 돌아감
+// 			currentIndex = secArr[0];
+// 		}
+// 		$('html,body')
+// 			.stop()
+// 			.animate({ scrollLeft: contents.eq(currentIndex).offset().left }, 'easeOutSine');
+
+// 		event.preventDefault();
+// 	} else if (event.originalEvent.deltaY < 0) {
+// 		// 마우스 휠 위로 스크롤
+// 		currentIndex--;
+// 		if (currentIndex < secArr[0]) {
+// 			// 첫번째 요소를 넘어가면 마지막 요소로 돌아감
+// 			currentIndex = secArr[secArr.length - 1];
+// 		}
+// 		$('html,body')
+// 			.stop()
+// 			.animate({ scrollLeft: contents.eq(currentIndex).offset().left }, 'easeOutSine');
+// 		event.preventDefault();
+// 	}
+// });
+
+// creators
+
+$(window).on('scroll', () => {
+	let winSCT;
+	const sections = $('section.creators');
+	winSCT = $(window).scrollTop();
+	sections.each(function (idx, o) {
+		$(o).addClass(`bg${idx + 1}`);
+		const tg = $(this);
+		const tgtop = tg.offset().top;
+		if (winSCT > tgtop) {
+			tg.find('.c_box::before').css('transform', 'translateY(0%)');
+		} else if (winSCT > tgtop) {
+			tg.find('.c_box::before').css('transform', 'translateY(0%)');
+		} else if (winSCT > tgtop) {
+			tg.find('.c_box::before').css('transform', 'translateY(0%)');
+		}
+	});
+});
